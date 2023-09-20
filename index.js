8c2316aea4ae8a156568b5da948356ca91bc9b64
Nirman <nirmanraj0512@gmail.com>
2023-09-20 12:19:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function changeText1(color) {
+    var yellow = document.getElementById('yellow');
+    yellow.textContent = color;
+}     
+function changeText2(color) {
+    var green = document.getElementById('green');
+    green.textContent = color;
+}    
+function changeText3(color) {
+    var blue = document.getElementById('blue');
+    blue.textContent = color;
+}    
+function changeText4(color) {
+    var red = document.getElementById('red');
+    red.textContent = color;
+}   
+
